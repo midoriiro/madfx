7848f0a3cce09ef2198895e45f68ec5984c11ee4
Alexis Bekhdadi <alexis.bekhdadi@gmail.com>
2020-11-10 11:52:56 +0100

    move audiofx library to root library folder

---
+++ library/core-audiofx/src/main/kotlin/midoriiro/io/core/audiofx/effects/Equalizer.kt
@@ -0,0 +1,10 @@
+package midoriiro.io.core.audiofx.effects
+
+import android.media.audiofx.AudioEffect
+
+class Equalizer(
+	priority: Int,
+	audioSession: Int
+) : AudioEffect(priority, audioSession)
+{
+}
